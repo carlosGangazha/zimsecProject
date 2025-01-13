130cf1cffa74871869e5b47feda3c9efd55f444c
Carlos Gangazha <carlosgangazha1@gmail.com>
2025-01-13 15:42:39 +0200

    small change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,13 +12,13 @@ export default function Footer(){
       <a href="/" className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
         <svg className="bi me-2" width="40" height="32"></svg>
       </a>
-      <p className="text-body-secondary">© 2024</p>
+      <p className="text-body-secondary">© 2025</p>
     </div>
 
   
 
     <div className="col mb-3">
-      <h5>Section</h5>
+      <h5>About</h5>
       <ul className="nav flex-column">
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
